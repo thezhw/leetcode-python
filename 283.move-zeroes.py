5435e984727f9678f00c22790bac0e1e16a11a8f
zhw <thezhw@gmail.com>
2017-02-11 10:10:21 +0800

    283

---
+++ 283.move-zeroes.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
+
+        print(nums)
+
+
+solution = Solution()
+solution.moveZeroes([0, 1, 0, 3, 12])
