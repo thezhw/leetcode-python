30871649fbe9e02cecddfbcb1fdb69e479c9095a
zhw <thezhw@gmail.com>
2017-02-13 20:15:43 +0800

    136

--- 136.single-number.py
+++ 136.single-number.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# 列表操作
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         no_dup_list = []
@@ -13,6 +14,29 @@ class Solution:
         return no_dup_list.pop()
 
 
-solution = Solution()
+# 哈希
+class Solution1:
+    def singleNumber(self, nums: List[int]) -> int:
+        no_dup_hash = {}
+        for i in nums:
+            try:
+                no_dup_hash.pop(i)
+            except KeyError:
+                no_dup_hash[i] = 1
+
+        return no_dup_hash.popitem()[0]
+
+
+# 异或
+class Solution2:
+    def singleNumber(self, nums: List[int]) -> int:
+        single = 0
+        for num in nums:
+            single ^= num
+
+        return single
+
+
+solution = Solution2()
 print(solution.singleNumber([2, 2, 1]))
 print(solution.singleNumber([4, 1, 2, 1, 2]))
