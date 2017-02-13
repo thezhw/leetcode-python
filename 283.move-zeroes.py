26201b59bf712a2608b420cfb90452431fb1f68a
zhw <thezhw@gmail.com>
2017-02-13 20:27:15 +0800

    283

--- 283.move-zeroes.py
+++ 283.move-zeroes.py
@@ -1,12 +1,13 @@
 from typing import List
 
 
+# 双指针
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = j = 0
+        j = 0
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[j], nums[i] = nums[i], nums[j]
