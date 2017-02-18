db19df83b6f8b97ca894e0408e6eeb9ab8f9a223
zhw <thezhw@gmail.com>
2017-02-18 20:45:02 +0800

    28

---
+++ 28.implement-strstr.py
@@ -0,0 +1,17 @@
+# 函数库
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
+
+
+# 暴力
+class Solution1:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+
+        return -1
