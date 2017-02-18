3a332e61cbf1f42dbf304d7fc4f3ef276d93639b
zhw <thezhw@gmail.com>
2017-02-18 18:35:48 +0800

    387

---
+++ 387.first-unique-character-in-a-string.py
@@ -0,0 +1,12 @@
+import collections
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = collections.Counter(s)
+
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
+
+        return -1
