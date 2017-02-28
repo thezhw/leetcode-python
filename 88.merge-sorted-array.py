8ec8f608462d05519104356d277e64f55928a9fc
zhw <thezhw@gmail.com>
2017-02-28 20:21:39 +0800

    88

---
+++ 88.merge-sorted-array.py
@@ -0,0 +1,56 @@
+from typing import List
+
+
+# 函数库
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m] + nums2)
+
+
+# 双指针 从前往后
+class Solution1:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        nums1_copy = nums1[:m]
+        nums1[:] = []
+
+        p1 = 0
+        p2 = 0
+
+        while p1 < m and p2 < n:
+            if nums1_copy[p1] < nums2[p2]:
+                nums1.append(nums1_copy[p1])
+                p1 += 1
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+
+        if p1 < m:
+            nums1[p1 + p2:] = nums1_copy[p1:]
+        if p2 < n:
+            nums1[p1 + p2:] = nums2[p2:]
+
+
+# 双指针 从后往前
+class Solution2:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] < nums2[p2]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            else:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            p -= 1
+
+        nums1[:p2 + 1] = nums2[:p2 + 1]
+
+
+solution = Solution()
+solution.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
