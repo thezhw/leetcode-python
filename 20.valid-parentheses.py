8c3f41373c68d9dfef600ecee84544469eb033a7
zhw <thezhw@gmail.com>
2017-03-16 14:34:00 +0800

    20

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                top = stack.pop() if stack else '#'
+                if top != mapping[char]:
+                    return False
+
+            else:
+                stack.append(char)
+
+        return not stack
