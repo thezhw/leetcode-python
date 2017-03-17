85a1d91056183aa3ac2f4f1cac24cb4b3ef57a88
zhw <thezhw@gmail.com>
2017-03-17 20:13:00 +0800

    268

---
+++ 268.missing-number.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        n = len(nums) + 1
+
+        for number in range(n):
+            if number not in num_set:
+                return number
+
+        return -1
