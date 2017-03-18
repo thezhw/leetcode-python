cb062c48c9778873fcfaef888e55a730a688f805
zhw <thezhw@gmail.com>
2017-03-18 09:02:27 +0800

    658

--- 658.find-k-closest-elements.py
+++ 658.find-k-closest-elements.py
@@ -16,5 +16,23 @@ class Solution:
         return arr[left:left + k]
 
 
-solution = Solution()
-print(solution.findClosestElements([1, 2, 3, 4, 5], 4, 3))
+class Solution1:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        size = len(arr)
+        left, right = 0, size - k
+
+        while left < right:
+            mid = (left + right) // 2
+
+            # 在 [mid, mid + k] 区间内 x 离右端点更近
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+
+        return arr[left:left + k]
+
+
+solution = Solution1()
+print(solution.findClosestElements([0, 1, 2, 3, 4, 5, 6, 7], 3, 6))
+print(solution.findClosestElements([0, 1, 2, 3, 4, 5, 6, 7], 3, 1))
