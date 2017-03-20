e9f311c4294b77246d2d523df3e10e35d34510a8
zhw <thezhw@gmail.com>
2017-03-20 20:24:08 +0800

    704

--- 704.binary-search.py
+++ 704.binary-search.py
@@ -19,5 +19,34 @@ class Solution:
         return -1
 
 
-solution = Solution()
-print(solution.search([5], 5))
+class Solution1:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2  # mid 取左中位数
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+
+        return -1 if nums[left] != target else left
+
+
+class Solution2:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = (left + right + 1) // 2  # mid 取右中位数
+            print(mid, nums[mid])
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid
+
+        return -1 if nums[left] != target else left
+
+
+solution = Solution1()
+print(solution.search([0, 1, 2, 3, 4, 5], 0))
